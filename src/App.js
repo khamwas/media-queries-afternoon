ee283e93e8d39dffc8e2a00e0a9044007f02fc18
Emmanuel Bravo <emmanuel@Emmanuels-MBP.attlocal.net>
2018-10-22 19:57:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,116 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import { push as Menu } from "react-burger-menu";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menu: false,
+      scroll: false
+    };
+    this.scrollChange = this.scrollChange.bind(this);
+  }
+
+  changeMenu() {
+    this.setState({ menu: !this.state.menu });
+  }
+
+  scrollChange() {
+    this.setState({ scroll: !this.state.scroll });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <nav id="n" className={this.state.scroll ? "scrollNav" : "nav"}>
+          <div className="nav">
+            <div className="logo">Start Bootstrap</div>
+            <button onClick={() => this.changeMenu()} className="menu">
+              MENU ☰
+            </button>
+          </div>
+          <div className={this.state.menu ? "dropStyle links" : "links"}>
+            <span className="linkItem">SERVICES</span>
+            <span className="linkItem">PORTFOLIO</span>
+            <span className="linkItem">ABOUT</span>
+            <span className="linkItem">TEAM</span>
+            <span className="linkItem">CONTACT</span>
+          </div>
+        </nav>
+        <header>
+          <div className="cursive">Welcome To Our Studio!</div>
+          <div className="headerText">IT'S NICE TO MEET YOU</div>
+          <button className="headerButton">TELL ME MORE</button>
         </header>
+        <div className="body">
+          <div className="services">
+            <h1>SERVICES</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <div className="iconContainer">
+              <div className="serviceItems">
+                <div className="icon" />
+                <div className="miniHeader">E-Commerce</div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Minima maxime quam architecto quo inventore harum ex magni,
+                  dicta impedit.
+                </div>
+              </div>
+              <div className="serviceItems">
+                <div className="icon" />
+                <div className="miniHeader">Responsive Design</div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Minima maxime quam architecto quo inventore harum ex magni,
+                  dicta impedit.
+                </div>
+              </div>
+              <div className="serviceItems">
+                <div className="icon" />
+                <div className="miniHeader">Web Security</div>
+                <div>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Minima maxime quam architecto quo inventore harum ex magni,
+                  dicta impedit.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="services">
+            <h1>PORTFOLIO</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="services">
+            <h1>ABOUT</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="services">
+            <h1>OUR AMAZING TEAM</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="services">
+            <h1>CONTACT US</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
       </div>
     );
+  }
+}
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 100) {
+    document.getElementById("n").className = "scrollNav";
+    // // this.scrollChange();
+    // console.log("small");
+  } else {
+    document.getElementById("n").className = "nav";
+    // // this.scrollChange();
+    // console.log("big");
   }
 }
 
